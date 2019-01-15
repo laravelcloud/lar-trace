116b635e74f4727b3d50506dddbb6a54721802a6
jianyong <jianyong.me@gmail.com>
2019-01-15 16:08:48 +0800

    add config

--- config/trace.php
+++ config/trace.php
@@ -6,7 +6,8 @@ return [
     'enabled' => getenv('TRACE_ENABLED'),
     'endpoint_url' => getenv('TRACE_ENDPOINT_URL'),
     'rate' => getenv('TRACE_RATE'),
-    'service_name' => getenv('TRACE_SERVICE_NAME')
+    'service_name' => getenv('TRACE_SERVICE_NAME'),
+    'sql_bindings' => getenv('TRACE_SQL_BINDINGS')
 ];
 
 
